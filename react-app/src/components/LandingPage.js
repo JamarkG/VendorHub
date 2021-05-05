c3b4b93e55bb2c9ef87399c92a2f7e9a5d78d4e5
Mark Gregory <markgregory19@gmail.com>
2021-05-04 23:28:09 -0700

    landing page functionality crept to authenticated landing page component planning, currently working on Search component mapping to cards sans db fetch

--- react-app/src/components/LandingPage.js
+++ react-app/src/components/LandingPage.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 import { NavLink } from 'react-router-dom';
+import SignUpForm from "./auth/SignUpForm";
 import background from './images/VendorHubBackground.png';
 import './CSS/LandingPage.css';
 
 const LandingPage = () => {
 
+    const [modalRendering, setModalRendering] = useState(false);
+
+    const setModalRenderingTrue = () => {
+        setModalRendering(true)
+    };
+    const setModalRenderingFalse = () => {
+        setModalRendering(false)
+    };
+
     return (
-        <div>
+        <div id='modalHolder'>
             <div id='compHolderDiv'>
                 <img src={background} id='imgHolder'></img>
                 <div id='landingTextButton'>
                     <h3>Simplify your vendor discovery</h3>
-                    <NavLink to="/login" style={{ textDecoration: 'none' }}>
-                        <div id='tryButtonDiv'>
+                    <div>
+                        <button onClick={setModalRenderingTrue} style={{ textDecoration: 'none' }} id='tryButton'>
                             <p id='tryButtonP'>Try it free</p>
-                        </div>
-                    </NavLink>
+                        </button>
+                        <Modal isOpen={modalRendering} parentSelector={() => document.querySelector('#modalHolder')}>
+                            <button onClick={setModalRenderingFalse}>Close</button>
+                            <SignUpForm />
+                        </Modal>
+                    </div>
                 </div>
             </div>
         </div>
