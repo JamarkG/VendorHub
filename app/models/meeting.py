c6f0a8151e9055c6a19747a08dea2f0e5b65dcdd
Mark Gregory <markgregory19@gmail.com>
2021-05-06 13:48:13 -0700

    completed setup of meeting table and seeded and showing in postbird

--- app/models/meeting.py
+++ app/models/meeting.py
@@ -4,8 +4,8 @@ class Meeting(db.Model):
   __tablename__ = 'meetings'
 
   id = db.Column(db.Integer, primary_key = True)
-  sendUserId = db.Column(db.Integer, nullable = False, db.ForeignKey('users.id'))
-  recUserId = db.Column(db.Integer, nullable = False)
+  sendUserId = db.Column(db.Integer, db.ForeignKey('users.id'))
+  recUserId = db.Column(db.Integer, db.ForeignKey('users.id'))
   message = db.Column(db.String(500), nullable = False)
   accepted = db.Column(db.Boolean, nullable = False)
 
